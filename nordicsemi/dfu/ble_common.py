64bbf1fda5aa382a41ffd245a13abbd5711b5950
sfrank <sfrank@oo>
2020-02-15 16:12:20 +0100

    untested implementation attempt bleak
    also auto formated with black

---
+++ nordicsemi/dfu/ble_common.py
@@ -0,0 +1,277 @@
+import struct
+from uuid import UUID
+import logging
+from enum import Enum, IntEnum
+
+logger = logging.getLogger(__name__)
+
+
+class NordicSemiException(Exception):
+    pass
+
+
+class ValidationException(NordicSemiException):
+    pass
+
+
+class NrfDfuOperationError(NordicSemiException):
+    pass
+
+
+class _UUIDWithStrCmp(UUID):
+    """ Same as UUID but compares to string (not case sensitive) """
+
+    def __cmp__(self, other):
+
+        if not isinstance(other, UUID):
+
+            if isinstance(other, int):
+                return self.int - other
+
+            if isinstance(other, float):
+                return self.int - int(other)
+
+            try:
+                other = UUID(other)
+            except Exception as e:
+                logger.warning("{} != {}".format(self, other))
+                return -1
+
+        return self.int - other.int
+
+    def __eq__(self, other):
+        return self.__cmp__(other) == 0
+
+    def __ne__(self, other):
+        return self.__cmp__(other) != 0
+
+    def __gt__(self, other):
+        return self.__cmp__(other) > 0
+
+    def __lt__(self, other):
+        return self.__cmp__(other) < 0
+
+    def __ge__(self, other):
+        return self.__cmp__(other) >= 0
+
+    def __le__(self, other):
+        return self.__cmp__(other) <= 0
+
+
+def _dfu_uuid(n):
+    """ NRF DFU UUID """
+    base = "8EC9{:04x}-F315-4F60-9FB8-838830DAEA50"
+    return _UUIDWithStrCmp(base.format(n))
+
+
+def _std_uuid(n):
+    """ Bluetooth LE "standard" uuid """
+    base = "0000{:04x}-0000-1000-8000-00805F9B34FB"
+    return _UUIDWithStrCmp(base.format(n))
+
+
+class BLE_UUID:
+    """
+
+    """
+
+    # fmt: off
+    S_GENERIC_SERVICE            = _std_uuid(0x1800)
+    S_GENERIC_ATTRIBUTE          = _std_uuid(0x1801)
+    S_NORDIC_SEMICONDUCTOR_ASA   = _std_uuid(0xFE59)
+    S_GENERIC_ATTRIBUTE_PROFILE  = _std_uuid(0x1801)
+    # Buttonless characteristics. Buttonless DFU without bonds 	
+    C_DFU_BUTTONLESS_UNBONDED    = _dfu_uuid(0x0003)
+    # Secure Buttonless DFU characteristic with bond sharing from SDK 14 or newer.
+    C_DFU_BUTTONLESS_BONDED      = _dfu_uuid(0x0004)
+    # service changed characteristic
+    C_SERVICE_CHANGED            = _std_uuid(0x2A05)
+    # Commands with OP_CODE. aka CP_UUID 
+    C_DFU_CONTROL_POINT          = _dfu_uuid(0x0001)
+    # aka DP_UUID 
+    C_DFU_PACKET_DATA            = _dfu_uuid(0x0002)
+    # fmt: on
+
+
+class OBJ_TYPE(IntEnum):
+    """ 
+    object_type.
+    enum nrf_dfu_obj_type_t. excl. INVALID  =  0x00 
+    """
+
+    # fmt: off
+    COMMAND  =  0x01
+    DATA     =  0x02
+    # fmt: on
+
+
+class OP_CODE(IntEnum):
+    """ 
+    operation/command codes to control point characteristic.
+    names (more or less) according to C enum nrf_dfu_op_t (OP_CODE) 
+    """
+
+    # fmt: off
+    PROTOCOL_VERSION   =  0x00
+    OBJECT_CREATE      =  0x01 # aka CreateObject
+    PRN_SET            =  0x02 # aka RECEIPT_NOTIF_SET or setPRN
+    CRC_GET            =  0x03 # aka CalcChecSum
+    OBJECT_EXECUTE     =  0x04 # aka Execute
+    OBJECT_SELECT      =  0x06 # aka ReadObject
+    MTU_GET            =  0x07
+    OBJECT_WRITE       =  0x08
+    PING               =  0x09
+    HARDWARE_VERSION   =  0x0A
+    FIRMWARE_VERSION   =  0x0B
+    ABORT              =  0x0C
+    RESPONSE           =  0x60
+    INVALID            =  0xFF
+    # fmt: on
+
+
+class RES_CODE(IntEnum):
+    """ 
+    response codes from control point characteristic.
+    note: success is _not_ zero!
+    enum nrf_dfu_result_t (RES_CODE).
+    excluding INVALID =  0x00
+    """
+
+    # fmt: off
+    SUCCESS                  =  0x01
+    OP_CODE_NOT_SUPPORTED    =  0x02
+    INVALID_PARAMETER        =  0x03
+    INSUFFICIENT_RESOURCES   =  0x04
+    INVALID_OBJECT           =  0x05
+    UNSUPPORTED_TYPE         =  0x07
+    OPERATION_NOT_PERMITTED  =  0x08
+    OPERATION_FAILED         =  0x0A
+    EXT_ERROR                =  0x0B # aka ExtendedError
+    # fmt: on
+
+
+class EXT_ERROR(IntEnum):
+    """ 
+    extended error codes from control point characteristic.
+    enum nrf_dfu_ext_error_code_t (NRF_DFU_EXT_ERROR) 
+    """
+
+    # fmt: off
+    NO_ERROR              =  0x00
+    INVALID_ERROR_CODE    =  0x01
+    WRONG_COMMAND_FORMAT  =  0x02
+    UNKNOWN_COMMAND       =  0x03
+    INIT_COMMAND_INVALID  =  0x04
+    FW_VERSION_FAILURE    =  0x05
+    HW_VERSION_FAILURE    =  0x06
+    SD_VERSION_FAILURE    =  0x07
+    SIGNATURE_MISSING     =  0x08
+    WRONG_HASH_TYPE       =  0x09
+    HASH_FAILED           =  0x0A
+    WRONG_SIGNATURE_TYPE  =  0x0B
+    VERIFICATION_FAILED   =  0x0C
+    INSUFFICIENT_SPACE    =  0x0D
+    # fmt: on
+
+
+class FW_TYPE(IntEnum):
+    """ enum nrf_dfu_firmware_type_t (NRF_DFU_FIRMWARE_TYPE_) """
+
+    # fmt: off
+    SOFTDEVICE   =  0x00
+    APPLICATION  =  0x01
+    BOOTLOADER   =  0x02
+    # fmt: on
+
+
+def cp_txd_pack(opcode, **kwargs):
+    """ pack control point characteristic request/command/transmit data """
+
+    if opcode == OP_CODE.PRN_SET:
+        assert len(kwargs) == 1
+        prn = kwargs["prn"]  # '<H':uint16 (LE)
+        return struct.pack("<BH", opcode, prn)
+
+    if opcode == OP_CODE.OBJECT_SELECT:
+        assert len(kwargs) == 1
+        obj_type = kwargs["object_type"]  # '<B':uint8
+        obj_type = OBJ_TYPE(obj_type)  # raise ValueError if invalid
+        return struct.pack("<BB", opcode, obj_type)
+
+    if opcode == OP_CODE.OBJECT_CREATE:
+        assert len(kwargs) == 2
+        obj_type = kwargs["object_type"]  # B:uint8
+        obj_type = OBJ_TYPE(obj_type)  # raise ValueError if invalid
+        obj_size = kwargs["size"]  # '<I':uint32 (LE)
+        return struct.pack("<BBI", opcode, obj_type, obj_size)
+
+    assert len(kwargs) == 0
+    return struct.pack("<B", opcode)
+
+
+def cp_rxd_unpack(opcode, data, has_header=True):
+    """ unpack/parse control point characteristic response/received data """
+    if not isinstance(data, (bytes, bytearray)):
+        data = bytearray(data)
+
+    if has_header:
+        data = cp_rxd_parse_header(opcode, data)
+
+    if opcode == OP_CODE.OBJECT_SELECT:
+        (max_size, offset, crc) = struct.unpack("<III", data)  # '<I':uint32 (LE)
+        return {"max_size": max_size, "offset": offset, "crc": crc}
+
+    if opcode == OP_CODE.CRC_GET:
+        (offset, crc) = struct.unpack("<II", data)  # '<I':uint32 (LE)
+        return {"offset": offset, "crc": crc}
+
+    return data
+
+
+def cp_rxd_parse_header(opcode, data):
+    """ parse control point characteristic response/received header and verify success. 
+    returns payload with header removed """
+
+    if isinstance(opcode, OP_CODE):
+        tx_opcode = opcode
+    else:
+        tx_opcode = OP_CODE(opcode)
+
+    emsg = "Bad response for {} -".format(tx_opcode)
+    if len(data) < 3:
+        raise NrfDfuOperationError(
+            "{} incomplete response size {}".format(emsg, len(data))
+        )
+
+    try:
+        rx_opcode = OP_CODE(data[1])
+    except ValueError as e:
+        raise NrfDfuOperationError("{} {}".format(emsg, e))
+    if rx_opcode != tx_opcode:
+        raise NrfDfuOperationError(
+            "{} unexpected response opcode {}".format(emsg, rx_opcode)
+        )
+
+    try:
+        rescode = RES_CODE(data[2])
+    except ValueError as e:
+        raise NrfDfuOperationError("{} {}".format(emsg, e))
+
+    if rescode == RES_CODE.EXT_ERROR:
+        if len(data) < 4:
+            raise NrfDfuOperationError("{} missing ext_error code".format(emsg))
+
+        try:
+            ext_errcode = EXT_ERROR(data[3])
+        except ValueError as e:
+            raise NrfDfuOperationError("{} {}".format(emsg, e))
+
+        raise NrfDfuOperationError("{} {}".format(emsg, ext_errcode))
+
+    # note SUCCESS is not zero :(
+    if rescode != RES_CODE.SUCCESS:
+        raise NrfDfuOperationError("{} {}".format(emsg, rescode))
+
+    # success
+    return data[3:]
+
---
+++ nordicsemi/dfu/dfu_transport_bleak.py
@@ -0,0 +1,367 @@
+import asyncio
+from enum import Enum, IntEnum
+import logging
+import binascii
+import sys
+
+# TODO no wild card imports
+from ble_common import *
+
+from bleak import BleakClient, discover
+from bleak.exc import BleakError
+
+logger = logging.getLogger(__name__)
+
+
+class _ATimeoutEvent(asyncio.Event):
+    """ 
+    Same as asyncio.Event but wait has a timeout option like threading.Event 
+    """
+
+    async def wait(self, timeout=None):
+        """ return True on success, False on timeout """
+        if timeout is None:
+            await super().wait()
+            return True
+
+        try:
+            await asyncio.wait_for(super().wait(), timeout)
+        except asyncio.TimeoutError:
+            return False
+
+        return True
+
+
+class _ATimeoutQueue(asyncio.Queue):
+    """ 
+    Same as asyncio.Queue but get has a timeout option like queue.Queue 
+    but raises asyncio.TimeoutError and not queue.Empty Exception.
+    """
+
+    async def get(self, timeout=None):
+        """ on timeout raises asyncio.TimeoutError not queue.Empty """
+        if timeout is None:
+            return await super().get()
+        else:
+            return await asyncio.wait_for(super().get(), timeout)
+
+
+def enum2str(enumclass, val):
+    """
+    enumclass - a Enum class, either instance or class 
+    """
+    try:
+        return enumclass(val).name
+    except ValueError:
+        return "{}.<unknown {}>".format(enumclass.__name__, val)
+
+
+class DfuDevice:
+    """
+    class represents a device already in DFU
+    """
+
+    def __init__(self, *args, **kwargs):
+        address = kwargs.get("address")
+        if address is None:
+            raise ValueError("invalid address")
+
+        timeout = kwargs.get("timeout", 10)
+        self._bleclnt = BleakClient(address, timeout=timeout)
+
+        # TODO what packet_size? 20 seems small --> slow
+        # packet size ATT_MTU_DEFAULT - 3
+        # ATT_MTU_DEFAULT = driver.GATT_MTU_SIZE_DEFAULT
+        # #define GATT_MTU_SIZE_DEFAULT 23
+        self.packet_size = 20
+
+        self._evt_opcmd = _ATimeoutEvent()
+        self.prn = 0
+        self.RETRIES_NUMBER = 3
+
+    async def __aenter__(self):
+        await self._bleclnt.connect()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self._bleclnt.disconnect()
+
+    async def cp_cmd(self, opcode, **kwargs):
+        """ 
+        control point (cp) characteristic command - handles request, 
+        parses response and chech success.
+        returns payload (if any)
+        """
+        cpuuid = BLE_UUID.C_DFU_CONTROL_POINT
+        txdata = cp_txd_pack(opcode, **kwargs)
+        if not isinstance(txdata, bytearray):
+            # bytes object not supported in txdbus
+            txdata = bytearray(txdata)
+
+        self._evt_opcmd.clear()
+
+        rxdata = bytearray()
+
+        def response_handler(sender, data):
+            # sender is str. should be uuid!?
+            if sender != cpuuid:
+                logger.warning(
+                    "unexpected notify response \
+                        from {} expected {}".format(
+                        sender, cpuuid
+                    )
+                )
+                return
+            rxdata.extend(data)
+            logger.debug("cp_cmd RXD:{}".format(data))
+            self._evt_opcmd.set()
+
+        await self._bleclnt.start_notify(cpuuid, response_handler)
+        await self._bleclnt.write_gatt_char(cpuuid, txdata, response=True)
+
+        if not await self._evt_opcmd.wait(6):
+            raise NrfDfuOperationError(
+                "CP Operation response timeout {}".format(opcode)
+            )
+
+        return cp_rxd_unpack(opcode, rxdata)
+
+    async def dpkg_write(self, data):
+        await self._bleclnt.write_gatt_char(
+            BLE_UUID.C_DFU_PACKET_DATA, data, response=True
+        )
+
+    async def _validate_crc(self, crc, offset):
+        response = await self.cp_cmd(OP_CODE.CRC_GET)
+        if crc != response["crc"]:
+            raise ValidationException(
+                "Failed CRC validation.\n"
+                + "Expected: {} Received: {}.".format(crc, response["crc"])
+            )
+        if offset != response["offset"]:
+            raise ValidationException(
+                "Failed offset validation.\n"
+                + "Expected: {} Received: {}.".format(offset, response["offset"])
+            )
+
+    async def __stream_data(self, data, crc=0, offset=0):
+        """ write to package data characteristic (aka DP_UUID or data_point) in
+        chunks and verify success"""
+        logger.debug(
+            "BLE: Streaming Data: len:{0} offset:{1} crc:0x{2:08X}".format(
+                len(data), offset, crc
+            )
+        )
+
+        current_pnr = 0
+        for i in range(0, len(data), self.packet_size):
+            packet = data[i : i + self.packet_size]
+            # was: self.write_data_point(packet)
+            await self._bleclnt.write_gatt_char(
+                BLE_UUID.C_DFU_PACKET_DATA, packet, response=True
+            )
+            crc = binascii.crc32(packet, crc) & 0xFFFFFFFF
+            offset += len(packet)
+            current_pnr += 1
+            if self.prn == current_pnr:
+                current_pnr = 0
+                await self._validate_crc(crc, offset)
+
+        await self._validate_crc(crc, offset)
+
+        return crc
+
+    async def send_init_packet(self, init_packet):
+        async def try_to_recover():
+            if response["offset"] == 0 or response["offset"] > len(init_packet):
+                # There is no init packet or present init packet is too long.
+                return False
+
+            expected_crc = (
+                binascii.crc32(init_packet[: response["offset"]]) & 0xFFFFFFFF
+            )
+
+            if expected_crc != response["crc"]:
+                # Present init packet is invalid.
+                return False
+
+            if len(init_packet) > response["offset"]:
+                # Send missing part.
+                try:
+                    await self.__stream_data(
+                        data=init_packet[response["offset"] :],
+                        crc=expected_crc,
+                        offset=response["offset"],
+                    )
+                except ValidationException:
+                    return False
+
+            # was: self.__execute()
+            await self.cp_cmd(OP_CODE.OBJECT_EXECUTE)
+            return True
+
+        # was: response = self.__select_command()
+        response = await self.cp_cmd(
+            OP_CODE.OBJECT_SELECT, object_type=OBJ_TYPE.COMMAND
+        )
+        if len(init_packet) > response["max_size"]:
+            raise Exception("Init command is too long")
+
+        if await try_to_recover():
+            return
+
+        for r in range(self.RETRIES_NUMBER):
+            try:
+                # was: self.__create_command(len(init_packet))
+                await self.cp_cmd(
+                    OP_CODE.OBJECT_CREATE,
+                    object_type=OBJ_TYPE.COMMAND,
+                    size=len(init_packet),
+                )
+                await self.__stream_data(data=init_packet)
+                # was: self.__execute()
+                await self.cp_cmd(OP_CODE.OBJECT_EXECUTE)
+            except ValidationException:
+                pass
+            break
+        else:
+            raise NordicSemiException("Failed to send init packet")
+
+    async def send_firmware(self, firmware):
+        async def try_to_recover():
+            if response["offset"] == 0:
+                # Nothing to recover
+                return
+
+            expected_crc = binascii.crc32(firmware[: response["offset"]]) & 0xFFFFFFFF
+            remainder = response["offset"] % response["max_size"]
+
+            if expected_crc != response["crc"]:
+                # Invalid CRC. Remove corrupted data.
+                response["offset"] -= (
+                    remainder if remainder != 0 else response["max_size"]
+                )
+                response["crc"] = (
+                    binascii.crc32(firmware[: response["offset"]]) & 0xFFFFFFFF
+                )
+                return
+
+            if (remainder != 0) and (response["offset"] != len(firmware)):
+                # Send rest of the page.
+                try:
+                    to_send = firmware[
+                        response["offset"] : response["offset"]
+                        + response["max_size"]
+                        - remainder
+                    ]
+                    response["crc"] = await self.__stream_data(
+                        data=to_send, crc=response["crc"], offset=response["offset"]
+                    )
+                    response["offset"] += len(to_send)
+                except ValidationException:
+                    # Remove corrupted data.
+                    response["offset"] -= remainder
+                    response["crc"] = (
+                        binascii.crc32(firmware[: response["offset"]]) & 0xFFFFFFFF
+                    )
+                    return
+
+            # was: self.__execute()
+            await self.cp_cmd(OP_CODE.OBJECT_EXECUTE)
+            # was: self._send_event(event_type=DfuEvent.PROGRESS_EVENT, progress=response['offset'])
+            logger.info("progress at {}".format(response["offset"]))
+
+        # was: response = self.__select_data()
+        response = await self.cp_cmd(OP_CODE.OBJECT_SELECT, object_type=OBJ_TYPE.DATA)
+        await try_to_recover()
+
+        for i in range(response["offset"], len(firmware), response["max_size"]):
+            data = firmware[i : i + response["max_size"]]
+            for r in range(self.RETRIES_NUMBER):
+                try:
+                    # was: self.__create_data(len(data))
+                    await self.cp_cmd(
+                        OP_CODE.OBJECT_CREATE, object_type=OBJ_TYPE.DATA, size=len(data)
+                    )
+                    response["crc"] = await self.__stream_data(
+                        data=data, crc=response["crc"], offset=i
+                    )
+                    # was: self.__execute()
+                    await self.cp_cmd(OP_CODE.OBJECT_EXECUTE)
+                except ValidationException:
+                    pass
+                break
+            else:
+                raise NordicSemiException("Failed to send firmware")
+            # was: self._send_event(event_type=DfuEvent.PROGRESS_EVENT, progress=len(data))
+            logger.info("progress at {}".format(len(data)))
+
+
+async def scan_dfu_devices(timeout=10, **kwargs):
+    devices = []
+    candidates = await discover(timeout=timeout)
+    for d in candidates:
+        match = None
+        if "uuids" in d.metadata:
+            advertised = d.metadata["uuids"]  # service uuids
+            # logger.debug(str(BLE_UUID.S_NORDIC_SEMICONDUCTOR_ASA) + " in " + str(advertised))
+            if BLE_UUID.S_NORDIC_SEMICONDUCTOR_ASA in advertised:
+                match = "nordic semi asa"
+            elif BLE_UUID.C_DFU_BUTTONLESS_BONDED in advertised:
+                match = "DFU bonded"
+            elif BLE_UUID.C_DFU_BUTTONLESS_UNBONDED in advertised:
+                match = "DFU unbonded"
+
+        if match:
+            logger.info(
+                "dfu device: {}  rssi:{} dBm  name:{} ({})".format(
+                    d.address, d.rssi, d.name, match
+                )
+            )
+            devices.append(d)
+        else:
+            logger.debug("ignoring device={}".format(d))
+
+    return devices
+
+
+def set_verbose(verbose_level):
+    loggers = [logging.getLogger("nrfdfu_defs"), logger]
+
+    if verbose_level <= 0:
+        level = logging.WARNING
+    elif verbose_level == 2:
+        level = logging.INFO
+    elif verbose_level >= 3:
+        level = logging.DEBUG
+
+    if verbose_level >= 4:
+        bleak_logger = logging.getLogger("bleak")
+        loggers.append(bleak_logger)
+
+    handler = logging.StreamHandler(sys.stderr)
+    handler.setLevel(level)
+
+    formatter = logging.Formatter("%(levelname)s:%(name)s:%(lineno)d: %(message)s")
+    handler.setFormatter(formatter)
+
+    for l in loggers:
+        l.setLevel(level)
+        l.addHandler(handler)
+
+
+async def run(address):
+    async with DfuDevice(address) as dev:
+        pass
+
+
+def main():
+    set_verbose(3)
+    logger.debug("Starting")
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(scan_dfu_devices())
+
+
+if __name__ == "__main__":
+    main()
+
